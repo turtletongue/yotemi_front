cb0f9de60daed46d99a2fd1448ff402a1d38056b
turtletongue <platon.s14@list.ru>
2023-03-16 19:08:05 +0500

    feat: add project description to homepage

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -19,17 +19,20 @@ const Home = async ({ params: { lang } }: HomeProps) => {
 
   return (
     <section className="grow bg-cetacean-blue flex items-center w-full px-4 overflow-hidden">
-      <div className="w-full md:w-1/2 h-full flex justify-center items-center text-white relative">
+      <div className="w-full lg:w-1/2 h-full flex justify-center items-center text-white relative">
         <FirstFigure className="absolute -top-48 left-48 motion-safe:animate-spin-slow" />
         <SecondFigure className="absolute top-72 left-16 motion-safe:animate-spin-slow" />
         <ThirdFigure className="absolute top-60 right-28 hidden lg:block motion-safe:animate-spin-slow" />
         <article className="flex flex-col items-center select-none">
-          <h1 className="flex flex-col text-center items-center font-black text-5xl sm:text-6xl">
+          <h1 className="flex flex-col text-center items-center font-black text-4xl sm:text-5xl 2xl:text-6xl">
             {translation("heading.normal")}{" "}
-            <span className="bg-title-blue-gradient bg-clip-text text-transparent h-20">
+            <span className="bg-title-blue-gradient bg-clip-text text-transparent">
               {translation("heading.colored")}
             </span>
           </h1>
+          <p className="my-4 text-sm sm:text-md text-center uppercase tracking-widest max-w-xs sm:max-w-md">
+            {translation("description")}
+          </p>
           <Button
             className="w-40 h-12 sm:w-48 sm:h-14 sm:mt-2 font-bold"
             animated
@@ -38,16 +41,17 @@ const Home = async ({ params: { lang } }: HomeProps) => {
           </Button>
         </article>
       </div>
-      <div className="hidden md:block relative w-1/2 h-full flex justify-center items-center text-white">
+      <div className="hidden lg:block relative w-1/2 h-full flex justify-center items-center text-white">
         <FourthFigure className="absolute -top-20 motion-safe:animate-spin-slow" />
-        <Image
-          className="relative"
-          src={MacMock}
-          alt={translation("mock")}
-          width={600}
-          unoptimized
-          priority
-        />
+        <div className="w-[48vw] h-[40vw] 2xl:w-[37vw] 2xl:h-[30vw] object-cover relative">
+          <Image
+            className="relative"
+            src={MacMock}
+            alt={translation("mock")}
+            priority
+            fill
+          />
+        </div>
       </div>
     </section>
   );
