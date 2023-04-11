9e83abae083ac173b6adeedcf2ab1e487a57d20c
turtletongue <platon.s14@list.ru>
2023-04-11 13:52:17 +0500

    fix: use tsconfig paths in production

---
+++ tsconfig-paths-bootstrap.js
@@ -0,0 +1,21 @@
+const tsConfigPaths = require("tsconfig-paths");
+
+const tsConfig = require("./tsconfig.json");
+
+const paths = tsConfig.compilerOptions.paths;
+
+tsConfigPaths.register({
+  baseUrl: tsConfig.compilerOptions.outDir,
+  paths: Object.keys(paths).reduce(
+    (agg, key) => ({
+      ...agg,
+      [key]: paths[key].map((p) =>
+        p.replace(
+          tsConfig.compilerOptions.baseUrl,
+          tsConfig.compilerOptions.outDir
+        )
+      ),
+    }),
+    {}
+  ),
+});
