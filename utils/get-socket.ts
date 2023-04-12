6bce34b3762451d67e460b2a87cf83126821533d
turtletongue <platon.s14@list.ru>
2023-04-12 22:49:12 +0500

    fix: use socket url instead of api url in socket.io

--- utils/get-socket.ts
+++ utils/get-socket.ts
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 import { getRefreshedToken } from "@utils";
 
 const getSocket = async () => {
-  return io(process.env.NEXT_PUBLIC_API_URL ?? "", {
+  return io(process.env.NEXT_PUBLIC_SOCKET_URL ?? "", {
     auth: {
       token: await getRefreshedToken(),
     },
