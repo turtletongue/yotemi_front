65c78849cf1e16c567cf210e94566347fa3f837e
turtletongue <platon.s14@list.ru>
2023-04-12 21:37:22 +0500

    fix: add path for socket.io

--- utils/get-socket.ts
+++ utils/get-socket.ts
@@ -4,6 +4,7 @@ import { getRefreshedToken } from "@utils";
 
 const getSocket = async () => {
   return io(process.env.NEXT_PUBLIC_API_URL ?? "", {
+    path: "/api/socket.io",
     auth: {
       token: await getRefreshedToken(),
     },
