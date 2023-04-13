0bc777f3182d413ac07e3602710c5c782a6ff1f3
turtletongue <platon.s14@list.ru>
2023-04-13 18:34:21 +0500

    fix: fix response type for exchangePeerId

--- redux/features/peers/peers.api.ts
+++ redux/features/peers/peers.api.ts
@@ -4,33 +4,31 @@ import { getSocket } from "@utils";
 
 const peersApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
-    exchangePeerId: builder.query<{ peerId: string; otherPeerId?: string }, Id>(
-      {
-        query: (id) => ({
-          url: `peers/${id}`,
-          method: "POST",
-        }),
-        providesTags: [{ type: "Peers", id: "PEER" }],
-        onCacheEntryAdded: async (arg, api) => {
-          try {
-            await api.cacheDataLoaded;
-            const socket = await getSocket();
+    exchangePeerId: builder.query<{ peerId: string; otherPeerId: string }, Id>({
+      query: (id) => ({
+        url: `peers/${id}`,
+        method: "POST",
+      }),
+      providesTags: [{ type: "Peers", id: "PEER" }],
+      onCacheEntryAdded: async (arg, api) => {
+        try {
+          await api.cacheDataLoaded;
+          const socket = await getSocket();
 
-            socket.on("peer.created", (data) => {
-              api.updateCachedData((draft) => {
-                draft[data.type === "own" ? "peerId" : "otherPeerId"] =
-                  data.peerId;
-              });
+          socket.on("peer.created", (data) => {
+            api.updateCachedData((draft) => {
+              draft[data.type === "own" ? "peerId" : "otherPeerId"] =
+                data.peerId;
             });
+          });
 
-            await api.cacheEntryRemoved;
-            socket.close();
-          } catch (error: unknown) {
-            console.error(error);
-          }
-        },
-      }
-    ),
+          await api.cacheEntryRemoved;
+          socket.close();
+        } catch (error: unknown) {
+          console.error(error);
+        }
+      },
+    }),
   }),
 });
 
