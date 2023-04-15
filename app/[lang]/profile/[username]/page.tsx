a4b6e4868d1de8f4e9a10740bdaa45d73e3f0a51
turtletongue <platon.s14@list.ru>
2023-04-16 00:37:20 +0500

    fix: change mobile elements widths

--- app/[lang]/profile/[username]/page.tsx
+++ app/[lang]/profile/[username]/page.tsx
@@ -78,7 +78,7 @@ const Profile = async ({ params: { lang, username } }: ProfileProps) => {
             </div>
             <Calendar lang={lang} user={profile} contractCode={contractCode} />
           </article>
-          <article className="flex flex-col w-full md:w-[33rem] 2xl:w-96 2xl:px-6 lg:px-0">
+          <article className="flex flex-col w-full md:w-auto md:max-w-[33rem]">
             <div className="flex w-full justify-between items-center">
               <span>{translation("reviews")}</span>
               <div className="flex">
