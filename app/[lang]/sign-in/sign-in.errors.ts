e64ad23bf6b7763a2e2715b812d56490cf9f7517
turtletongue <platon.s14@list.ru>
2023-04-17 00:10:57 +0500

    feat: handle walletNotInitialized error

--- app/[lang]/sign-in/sign-in.errors.ts
+++ app/[lang]/sign-in/sign-in.errors.ts
@@ -2,6 +2,7 @@ import { generalErrors } from "@utils";
 
 const signUpErrors = {
   401: "unauthorizedError",
+  TON_ACCOUNT_NOT_INITIALIZED: "walletNotInitialized",
   ...generalErrors,
 } as const;
 
