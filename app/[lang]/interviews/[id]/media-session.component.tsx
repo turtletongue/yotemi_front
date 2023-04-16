d06836f0f06863bcdeb4bc5f6642c40d4a3e3e50
turtletongue <platon.s14@list.ru>
2023-04-17 01:17:00 +0500

    remove debug

--- app/[lang]/interviews/[id]/media-session.component.tsx
+++ app/[lang]/interviews/[id]/media-session.component.tsx
@@ -221,8 +221,6 @@ const MediaSession = ({ lang, interview }: MediaSessionProps) => {
     authenticatedUser?.id === interview.participant?.id;
   const isInterviewFinished = new Date(interview.endAt) < new Date();
 
-  console.log(authenticatedUser, interview, isInterviewFinished);
-
   if (!authenticatedUser || !isParticipant || isInterviewFinished) {
     return redirect("/");
   }
