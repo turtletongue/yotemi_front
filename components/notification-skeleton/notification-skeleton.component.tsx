65c862d95711342b6ff33e3e322fd266dc559d42
turtletongue <platon.s14@list.ru>
2023-05-25 13:03:25 +0500

    fix: make notification skeleton more rounded

--- components/notification-skeleton/notification-skeleton.component.tsx
+++ components/notification-skeleton/notification-skeleton.component.tsx
@@ -2,7 +2,7 @@ const NotificationSkeleton = () => {
   return (
     <div className="flex w-full justify-center items-center py-5 animate-pulse">
       <div className="mx-3 h-10 w-10 rounded-full bg-gray-400" />
-      <div className="h-2.5 w-[19rem] bg-gray-400" />
+      <div className="h-2.5 w-[19rem] bg-gray-400 rounded-full" />
     </div>
   );
 };
