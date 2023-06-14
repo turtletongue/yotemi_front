0e5d3b7db4734d2c60c13c0e0d59e444b42480d1
turtletongue <platon.s14@list.ru>
2023-06-14 22:01:01 +0500

    fix: set x padding to notification skeleton

--- components/notification-skeleton/notification-skeleton.component.tsx
+++ components/notification-skeleton/notification-skeleton.component.tsx
@@ -1,6 +1,6 @@
 const NotificationSkeleton = () => {
   return (
-    <div className="flex w-full justify-center items-center py-5 animate-pulse">
+    <div className="flex w-full items-center px-2 py-5 animate-pulse">
       <div className="mx-3 h-10 w-10 rounded-full bg-gray-400" />
       <div className="h-2.5 w-[19rem] bg-gray-400 rounded-full" />
     </div>
